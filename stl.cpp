b6ed6c63ef8a3591dbdcc48f0fb8e368de5a9164
NASA_Mark <76894588+Atharane@users.noreply.github.com>
2022-08-15 11:35:26 +0530

    code sets & unordered_sets

---
+++ stl.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+
+    // Unordered Set
+    
+    /*
+    {
+        unordered_set<string> language;
+
+        language.insert("lorem");
+        language.insert("ipsum");
+        language.insert("amet");
+
+        cout << language.count("dolor") << endl;
+
+        if (language.find("ipsum") != language.end())
+            cout << *(language.find("ipsum")) << " found!" << endl;
+        else
+            cout << "Element not found!";
+
+        language.erase("lorem");
+        language.erase(language.begin());
+        cout << "New set size: " << language.size() <<endl;
+
+        for (auto it = language.begin(); it != language.end(); it++)
+        {
+            cout << *it << " ";
+        }
+        cout << endl;
+
+        // deletes all the elements in unordered set
+        language.erase(language.begin(), language.end());
+
+        cout << "is unordered set empty: " << language.empty() << endl;
+
+        // deletes all the elements in unordered set
+        language.clear();
+    }
+    */
+
+    // Sets
+    /*
+    {
+        set<char> alphabets;
+
+        alphabets.insert('a');
+        alphabets.insert('b');
+        alphabets.insert('c');
+        alphabets.insert('d');
+        alphabets.insert('e');
+
+        cout << "Size of set: " << alphabets.size() << endl;
+        for(auto it = alphabets.begin(); it != alphabets.end(); it++)
+        {
+            cout << *it << " ";
+        }
+    }  
+    */  
+
+    return 0;
+}
